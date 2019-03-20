333f74090496a12bd740fd1ea4b9bb281db75e1f
Reuel Sicat II <reuel@truelogic.com.ph>
2019-03-21 07:28:58 +0800

    Additional changes from Weng to reinforce understanding on Dependency Injection using Property Qualifiers w/ Primary

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,7 +1,6 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,10 +8,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Primary
-@Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
-    @Override
-    public String sayGreeting() {
-        return "Primärer Grußdienst";
-    }
+	@Override
+	public String sayGreeting() {
+		return "Primärer Grußdienst";
+	}
 }
--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,19 +1,17 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
 @Service
-@Profile("es")
-@Primary
+//@Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
-    @Override
-    public String sayGreeting() {
-        return "Servicio de Saludo Primario";
-    }
+	@Override
+	public String sayGreeting() {
+		return "Servicio de Saludo Primario";
+	}
 }
